d22dd819084b8f11b50ba2ce2e28077c2042fca9
Basil Shubin <basil.shubin@gmail.com>
2013-11-23 21:21:09 +0700

    fine-tune transactions

--- feedback_form/forms.py
+++ feedback_form/forms.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.db import transaction
 from django.contrib import messages
 from django.utils.translation import ugettext as _
 
@@ -9,5 +10,7 @@ from contact_form.forms import AkismetContactForm
 class FeedbackForm(AkismetContactForm):
 
     def save(self, fail_silently=False):
-        super(FeedbackForm, self).save(fail_silently)
-        messages.success(self.request, _("Your email was sent successfully!"))
+        with transaction.commit_on_success():
+            super(FeedbackForm, self).save(fail_silently)
+            messages.success(self.request, _(
+                "Your email was sent successfully!"))
