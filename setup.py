7616a2715b423851dea00f2ece902a68ea090b88
Basil Shubin <basil.shubin@gmail.com>
2015-09-27 10:24:49 +0600

    bump version number

--- setup.py
+++ setup.py
@@ -1,36 +1,79 @@
+#!/usr/bin/env python
+
 import os
-from setuptools import setup
+import re
+import sys
+import codecs
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+from setuptools import setup, find_packages
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv or 'develop' in sys.argv:
+    os.chdir('feedback_form')
+    try:
+        from django.core.management.commands.compilemessages import Command
+        command = Command()
+        command.execute(stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        # < Django 1.7
+        from django.core.management.commands.compilemessages import compile_messages
+        compile_messages(sys.stderr)
+    finally:
+        os.chdir('..')
+
+
+def read(*parts):
+    file_path = os.path.join(os.path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='django-feedback-form2',
-    version='0.1.7.1',
-    packages=['feedback_form'],
-    include_package_data=True,
+    version=find_version('feedback_form', '__init__.py'),
     license='BSD License',
-    description='Put short description here...',
-    long_description=README,
-    url='http://github.com/bashu/django-feedback-form',
-    author='Basil Shubin',
+
     install_requires=[
-        'django>=1.5',
-        'akismet>=0.2.0',
         'django-contact-form>=1.0',
     ],
+    requires=[
+        'Django (>=1.4.2)',
+    ],
+
+    description='Generic feedback-form application for Django',
+    long_description=read('README.rst'),
+
+    author='Basil Shubin',
     author_email='basil.shubin@gmail.com',
+
+    url='http://github.com/bashu/django-feedback-form',
+    download_url='https://github.com/bashu/django-feedback-form/zipball/master',
+
+    packages=find_packages(exclude=('example*', '*.tests*')),
+    include_package_data=True,
+
+    zip_safe=False,
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    zip_safe=False,
 )
