f7d4caa25e424c058074491aa928b1d7bba1fe0b
Basil Shubin <basil.shubin@gmail.com>
2015-10-06 08:55:41 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ def find_version(*parts):
 
 
 setup(
-    name='django-feedback-form2',
+    name='django-feedback-form-deux',
     version=find_version('feedback_form', '__init__.py'),
     license='BSD License',
 
